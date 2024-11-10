ad6343323bb7b5b369f55b2358e74e2f932903af
Sandi Romadhon <sandiromadon33@gmail.com>
2024-11-10 20:25:10 +0700

    upate

--- src/pages/Badge/List/index.tsx
+++ src/pages/Badge/List/index.tsx
@@ -1,4 +1,4 @@
-import { FaRegTrashCan } from 'react-icons/fa6';
+
 import Breadcrumb from '../../../components/Breadcrumbs/Breadcrumb';
 import CustomButton from '../../../components/Button/CustomButton';
 import CustomIconButton from '../../../components/Button/CustomIconButton';
